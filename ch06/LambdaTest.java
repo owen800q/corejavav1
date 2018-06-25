369881111a404ead9fc645151374935ad935e7f4
wika <owen800q@gmail.com>
2018-06-25 15:28:07 +0800

    local inner class

--- ch06/LambdaTest.java
+++ ch06/LambdaTest.java
@@ -1,4 +1,5 @@
 package corejava1.ch06;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -13,36 +14,58 @@ import javax.swing.Timer;
  */
 
 
- class LambdaTest
-{
+class LambdaTest {
     public static void main(String[] args) {
-        Employee[] staff = new Employee[3];
+        ArrayList<Employee> staff = new ArrayList<>();
 
-        staff[0] = new Employee("ddd", 35000);
-        staff[1] = new Employee("dd", 75000);
-        staff[2] = new Employee("d", 38000);
-        var newStaff = (Employee) (staff[0].clone());
-        newStaff.setName("debug");
-        System.out.println(newStaff.getName());
-        newStaff.sort(staff);
+        staff.add(new Employee("ddd", 35000));
+        staff.add(new Employee("dd", 75000));
+        staff.add(new Employee("d", 38000));
 
+
+        System.out.println("Before removal:");
         // print out information about all Employee objects
         for (Employee e : staff)
             System.out.println("name=" + e.getName() + ",salary=" + e.getSalary());
 
-
+        System.out.println("After removing the first obj.:");
+        staff.removeIf(e -> e.getName().equals("ddd"));
+        for (Employee e : staff)
+            System.out.println("name=" + e.getName() + ",salary=" + e.getSalary());
+                /*
         var t = new Timer(1000, event ->
         {
             System.out.println("At the tone, the time is " + new Date());
             Toolkit.getDefaultToolkit().beep();
         });
         t.start();
+
         JOptionPane.showMessageDialog(null, "Quit program?");
         System.exit(0);
+    */
+        //repreatMessage("Hello world", 1000);
+        //JOptionPane.showMessageDialog(null, "Quit program?");
+        //System.exit(0);
+        repeat(10,()->System.out.println("Hello world"+new Date()));
+
     }
 
 
+        public static void repreatMessage(String text, int delay) {
+           int i=0;
+            new Timer(delay, (ActionEvent event) ->
+            {
+                ;
+                System.out.println(text+new Date());
+                Toolkit.getDefaultToolkit().beep();
+            }).start();
+        }
 
+        public static void repeat(int n,Runnable action)
+        {
+            for(int i=0;i<n;i++)
+                action.run();
+        }
 }
 
 
